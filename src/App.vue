e642a9f44eaba50c13c240c0ae104439e47592fa
Manuel Marquez <80608697+manuelhm98@users.noreply.github.com>
2022-08-23 10:13:46 -0600

    Firts Commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <!-- <Counter :start="101" /> -->
+  <!-- <Counter tittle="End" /> -->
+  <Indecision />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Indecision from './components/Indecision.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Indecision,
+  },
 }
 </script>
 
---
+++ src/components/Counter.vue
@@ -0,0 +1,66 @@
+<template>
+  <h2>{{ customTittle }}</h2>
+  <p>{{ counter }} <sub>2</sub> = {{ squareCounter }}</p>
+  <div>
+    <button @click="increase">+ 1</button>
+    <button @click="decrease">- 1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  //props: ['tittle'],
+  props: {
+    tittle: String,
+    start: {
+      type: Number,
+      default: 1,
+      validator(value) {
+        return value > 100
+      },
+    },
+  },
+  data() {
+    return {
+      counter: this.start,
+    }
+  },
+  methods: {
+    getSquareValue() {
+      return this.counter * this.counter
+    },
+    increase() {
+      this.counter++
+    },
+    decrease() {
+      this.counter--
+    },
+  },
+  computed: {
+    squareCounter() {
+      return this.counter * this.counter
+    },
+    customTittle() {
+      return !this.tittle ? 'Counter' : this.tittle
+    },
+  },
+}
+</script>
+
+<style>
+button {
+  margin: 0 5px;
+  border-radius: 5px;
+  background-color: #64b687;
+  border: 1px solid white;
+  color: white;
+  cursor: pointer;
+  padding: 5px 15px;
+  transition: 0.3s ease-in-out;
+}
+
+button:hover {
+  background-color: #5aa67b;
+  transition: 0.3s ease-in-out;
+}
+</style>
